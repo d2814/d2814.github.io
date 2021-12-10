d4a6634bdbb0aeee056e5a7ca2ea1e0d55325067
d2814 <2814140602@qq.com>
2021-12-10 16:00:20 +0800

    Site updated: 2021-12-10 16:00:18

--- js/init.js
+++ js/init.js
@@ -175,6 +175,7 @@ function LoadPagerService() {
 			xhr.onreadystatechange = function(){
 				if(!domain.rootFlag && xhr.readyState == 4 && xhr.status == 200){
 					console.log(urlJson);
+					console.log("name: " + urlJson[i]['name']);
 					domain.rootFlag = true;
 					domain.supplier = urlJson[i]['name'];
 					domain.url = urlJson[i]['url'];
